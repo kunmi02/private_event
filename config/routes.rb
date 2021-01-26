ae633c7724e7130bf5c67a45857450c9b060e811
nna nna 19 <uchennaongit@gmail.com>
2021-01-26 13:41:01 +0100

    Create user controller and migration and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'users#index'
+  resources :users, only: %i[show new show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
