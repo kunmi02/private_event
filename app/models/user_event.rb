0b41d82ff013fc71fe825252c33df8906f9e1236
kunmi02 <mrkhunmie@gmail.com>
2021-02-03 19:35:10 +0100

    Fix wrong commit

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,4 +1,6 @@
 class UserEvent < ApplicationRecord
+  validates_uniqueness_of :event_id, :user_id
+
   belongs_to :attended_event, class_name: 'Event', foreign_key: 'event_id'
   belongs_to :attendee, class_name: 'User', foreign_key: 'user_id'
 end
