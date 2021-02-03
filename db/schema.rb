6ed1baf23b263f4b6de20c5e5549306ec18cb47b
kunmi02 <mrkhunmie@gmail.com>
2021-02-03 15:23:10 +0100

    fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_175214) do
-
+ActiveRecord::Schema.define(version: 20_210_202_175_214) do
   create_table 'events', force: :cascade do |t|
     t.string 'description'
     t.datetime 'date'
