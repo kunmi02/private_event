f9a765a6283d605bd91e77eda6dc58c46cf98660
kunmi02 <mrkhunmie@gmail.com>
2021-02-03 10:43:06 +0100

    modified show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
-  has_many :user_events # , foreign_key: 'event_id'
-  # has_many :attendees, through: :joiners
+  has_many :user_events
   has_many :attendees, through: :user_events
 
   scope :upcoming_events, -> { where('date > ?', Time.now) }
   scope :past_events, -> { where('date <= ?', Time.now) }
 end
-
-# event model
-#   belongs_to :user
-#   has_many :event_attendee, foreign_key: 'event_attended_id'
-#   has_many :attendees, through: :event_attendee
-
-#   scope :upcoming_events, -> { where('date > ?', Time.now) }
-#   scope :past_events, -> { where('date <= ?', Time.now) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
   has_many :events
-  # has_many :joiners
   has_many :user_events
   has_many :attended_events, through: :user_events
 end
-
-# user model
-# has_many :events
-# has_many :event_attendee, foreign_key: 'attendee_id'
-# has_many :event_attended, through: :event_attendee
